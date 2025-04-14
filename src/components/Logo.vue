a006d5c340c4589c7626abf1a257010f72a3fa21
Dilier <dilierestebanfortalechelopez@gmail.com>
2025-04-14 16:14:17 -0500

    componenteLogo

---
+++ src/components/Logo.vue
@@ -0,0 +1,41 @@
+<!-- src/components/Logo.vue -->
+<template>
+    <img
+      :src="src"
+      :alt="alt"
+      :class="['img-fluid', imgClass]"
+      :style="styleObject"
+    />
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    src: {
+      type: String,
+      default: '/src/assets/logo.png', // Ruta por defecto del logo
+    },
+    alt: {
+      type: String,
+      default: 'Logo',
+    },
+    width: {
+      type: [String, Number],
+      default: 100,
+    },
+    height: {
+      type: [String, Number],
+      default: 100,
+    },
+    imgClass: {
+      type: String,
+      default: '',
+    },
+  });
+  
+  const styleObject = {
+    width: `${props.width}px`,
+    height: `${props.height}px`,
+    objectFit: 'contain',
+  };
+  </script>
+  
